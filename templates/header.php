6f1fde45e6d89068ce5de3fcc50c864a1ffb2c9a
Caroline Shaw <caroline.shaw@gmail.com>
2015-11-29 22:03:33 -0500

    Added social media icons

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,10 @@
 <header class="banner clearfix">
-<ul class="social header-social">
-  <li class="social-instagram"><a href="http://instagram.com/colinsmithwoodworking">Instagram</a></li>
-  <li class="social-facebook"><a href="https://www.facebook.com/colinsmithwoodworking">Facebook</a></li>
-  <li class="social-email"><a href="mailto:info@colinsmithwoodworking.com">Email</a></li>
-</ul>
   <div class="container">
+  <ul class="social header-social">
+    <li class="social-instagram"><a href="http://instagram.com/colinsmithwoodworking">Instagram</a></li>
+    <li class="social-facebook"><a href="https://www.facebook.com/colinsmithwoodworking">Facebook</a></li>
+    <li class="social-email"><a href="mailto:info@colinsmithwoodworking.com">Email</a></li>
+  </ul>
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav class="nav-primary">
       <?php
