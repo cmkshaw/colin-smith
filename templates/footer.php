82fbf01fdd8ce450bb7ebfd6522d300c3177da71
Caroline Shaw <caroline.shaw@gmail.com>
2016-01-17 20:25:09 -0500

    Added social media icons to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,14 +1,18 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+
+  
   <?php 
    	if ( is_page_template('template-products.php') || is_singular('product')  ) {
   		get_template_part('templates/widget-product');
   	}
   ?>
   <div class="container copyright">
+    <ul class="social social-footer show-mobile">
+      <li class="social-instagram"><a href="http://instagram.com/colinsmithwoodworking">Instagram</a></li>
+      <li class="social-facebook"><a href="https://www.facebook.com/colinsmithwoodworking">Facebook</a></li>
+      <li class="social-email"><a href="mailto:info@colinsmithwoodworking.com">Email</a></li>
+    </ul>
     &copy; Colin Smith Woodworking <?php echo date("Y"); ?>
-    <a class="pull-right" href="/sitemap">Sitemap</a>
+    <a class="pull-right hide-mobile" href="/sitemap">Sitemap</a>
   </div>
 </footer>
